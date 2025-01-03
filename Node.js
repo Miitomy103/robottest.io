eeb966775b2f71cfb4531524bf69ed2bc791eb9d
Miitomy103 <tomy463463@gmail.com>
2025-01-04 02:04:50 +0900

    Add files via upload

--- Node.js
+++ Node.js
@@ -4,16 +4,17 @@ const fetch = require('node-fetch');
 
 const app = express();
 const PORT = 3000;
-
-// Google reCAPTCHA秘密鍵
 const SECRET_KEY = '6Lf6Ra0qAAAAABFTT_vigOF1uv2ngP-a41Xm5lWH';
 
 app.use(bodyParser.json());
 
 app.post('/verify-captcha', async (req, res) => {
-    const token = req.body.token;
+    const { token } = req.body;
 
-    // reCAPTCHAの検証リクエストを送信
+    if (!token) {
+        return res.json({ success: false, error: "トークンが提供されていません。" });
+    }
+
     const response = await fetch(`https://www.google.com/recaptcha/api/siteverify`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
@@ -21,14 +22,13 @@ app.post('/verify-captcha', async (req, res) => {
     });
 
     const data = await response.json();
-
     if (data.success) {
         res.json({ success: true });
     } else {
-        res.json({ success: false });
+        res.json({ success: false, error: data['error-codes'] });
     }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
